889161801fa3203992a49dbf90772354a13b207b
un-simp <uncraftablecreeper@gmail.com>
2024-04-07 22:48:37 +0100

    as

--- CWDoom/Hooks/FFmpegHook.cs
+++ CWDoom/Hooks/FFmpegHook.cs
@@ -14,11 +14,12 @@ public class FFmpegHook
 	internal static IEnumerator FfmpegEncoderOnRunFfmpeg(FfmpegEncoder.orig_RunFfmpeg orig, Zorro.Recorder.FfmpegEncoder self, string arguments, bool displaywindow)
 	{
 		
+		
 		GameObject display = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		MeshRenderer renderer = display.GetComponentInChildren<MeshRenderer>();
 		display.name = "Display";
 		display.transform.position = new Vector3((float) -21.5031, (float) 2.83913, (float) 16.0612);
-		display.transform.rotation =  Quaternion.Euler(0,(float) -135.221,0);
+		display.transform.rotation =  Quaternion.Euler(0,(float) 44.8001,0);
 		display.transform.localScale = new Vector3((float) 2.9277, (float) 1.7157, (float) 0.1608);
 		
 		Shader unlitTextureShader = Resources.FindObjectsOfTypeAll<Shader>().First((shader) => shader.name == "Universal Render Pipeline/Unlit");
